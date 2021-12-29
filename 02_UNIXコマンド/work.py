b71ceebd12ec22d6f8438638a2db580b4c52e972
schnell3526 <schnelldickheiter@gmail.com>
2021-12-29 12:50:07 +0900

    11問目に回答

--- 02_UNIXコマンド/work.py
+++ 02_UNIXコマンド/work.py
@@ -1,4 +1,5 @@
 import subprocess
+import re
 
 def ex10(file_name:str="popular-names.txt")->str:
   """ファイルの行数を表示"""
@@ -8,3 +9,15 @@ def ex10(file_name:str="popular-names.txt")->str:
     line_num_python = len([1 for _ in file])
     assert line_num_python == line_num_unix
     return line_num_python
+
+def ex11(file_name:str="popular-names.txt"):
+  """tabをスペースに置換"""
+  # cat popular-names.txt | sed "s/\t/ /g"
+  with open(file_name) as file, open("ex11.txt", "w") as fout:
+    for line in file:
+      line = re.sub(r"\t", " ", line)
+      fout.write(line)
+
+def ex12(file_name:str="popular-names.txt"):
+  """カラムの抜き出し"""
+  with open(file_name) as file, open("ex12.txt", "w") as fout:
