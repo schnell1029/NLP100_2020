03c34849cc2d8ec3eb06141b6be27e37402b4048
schnell3526 <schnelldickheiter@gmail.com>
2022-01-03 11:57:34 +0900

    修正

--- 03_正規表現/work.py
+++ 03_正規表現/work.py
@@ -16,7 +16,7 @@ def ex21(file_name="jawiki-country.json")->List:
   df = pd.read_json(file_name, lines=True)
   uk_text = df.query("title=='イギリス'")["text"].values[0]
 
-  pattern = re.compile(r".*Category.*")
+  pattern = re.compile(r"^.*\[Category.*$", re.MULTILINE)
   res = pattern.findall(uk_text)
   [print(m) for m in res]
   return
@@ -29,4 +29,26 @@ def ex22(file_name="jawiki-country.json"):
   pattern = re.compile(r"\[\[Category:(.*?)(|\|.*)\]\]")
   res = pattern.findall(text)
   [print(g[0]) for g in res]
+  return
+
+def ex23(file_name="jawiki-country.json"):
+  """セクション名とそのレベルを表示
+
+  例) レベル1
+    ==セクション名==
+  例) レベル2
+    ===セクション名===
+  """
+  df = pd.read_json(file_name, lines=True)
+  text = df.query("title=='イギリス'")["text"].values[0]
+
+  pattern = re.compile(r"(={2,})\s*(.+?)\s*(\1)")
+  res = pattern.findall(text)
+  for match in res:
+    level = len(match[0]) - 1
+    prefix = "  "*level + str(level)
+    print(prefix + match[1])
+  return
+
+def ex24(file_name="jawiki-country.json"):
   return
