8f702719279dd4a9f39a2252e1464501a2a5ab52
schnell3526 <schnelldickheiter@gmail.com>
2022-01-05 23:56:04 +0900

    26問目まで回答

--- 03_正規表現/work.py
+++ 03_正規表現/work.py
@@ -1,6 +1,4 @@
 import re
-import json
-from typing import List
 
 import pandas as pd
 
@@ -11,7 +9,7 @@ def ex20(file_name="jawiki-country.json"):
   print(uk)
   return
 
-def ex21(file_name="jawiki-country.json")->List:
+def ex21(file_name="jawiki-country.json"):
   """カテゴリ名を宣言している行を抽出"""
   df = pd.read_json(file_name, lines=True)
   uk_text = df.query("title=='イギリス'")["text"].values[0]
@@ -51,4 +49,46 @@ def ex23(file_name="jawiki-country.json"):
   return
 
 def ex24(file_name="jawiki-country.json"):
+  """記事から参照されているメディアファイルを全て抜き出す
+
+  例)
+    [[ファイル:Royal Coat of Arms of the United Kingdom.svg|85px|イギリスの国章]]
+  """
+  df = pd.read_json(file_name, lines=True)
+  text = df.query("title=='イギリス'")["text"].values[0]
+
+  pattern = re.compile(r"\[\[ファイル:(.+?)\|")
+  res = pattern.findall(text)
+  [print(item) for item in res]
   return
+
+def ex25(file_name="jawiki-country.json"):
+  """基礎情報テンプレートのフィールド名と値を抽出し、辞書オブジェクトとして格納
+
+  例)
+    [[ファイル:Royal Coat of Arms of the United Kingdom.svg|85px|イギリスの国章]]
+  """
+  df = pd.read_json(file_name, lines=True)
+  text = df.query("title=='イギリス'")["text"].values[0]
+
+  pattern = re.compile(r"\{\{基礎情報.*?\n(.*?)\n\}\}", re.DOTALL)
+  res = pattern.findall(text)
+
+  pattern = re.compile(r"\|(.+)\s=(.+)\n")
+  res = pattern.findall(res[0])
+  res = {key: value for key, value in res}
+  return res
+
+def ex26(file_name="jawiki-country.json"):
+  """強調マークアップの除去
+
+  例)
+    ''弱い強調''
+    '''強調'''
+    '''''強い強調'''''
+  """
+  dic = ex25()
+
+  pattern = re.compile(r"\'{2,5}")
+  res = {key: pattern.sub("", value) for key, value in dic.items()}
+  return res
